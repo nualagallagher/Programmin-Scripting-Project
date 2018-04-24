4022f0a0e7ed32404ecd455dbf52a2db656c464d
nualagallagher <36246540+nualagallagher@users.noreply.github.com>
2018-04-24 21:17:45 +0100

    Update project.py
    Opening the data set and printing the file.

--- project.py
+++ project.py
@@ -1,1 +1,21 @@
 
+
+f = open("data/iris.csv")
+
+
+#prints the name of the file
+print(f)
+
+
+#print the contents of the file
+
+print(f.read())f = open("data/iris.csv")
+
+
+#prints the name of the file
+print(f)
+
+
+#print the contents of the file
+
+print(f.read())
