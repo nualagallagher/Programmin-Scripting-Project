6f6a4a59a2519349293579efbeb1fbc81dd3750e
nualagallagher <36246540+nualagallagher@users.noreply.github.com>
2018-04-25 21:36:40 +0100

    Update project.py

--- project.py
+++ project.py
@@ -1,5 +1,3 @@
-
-
 f = open("data/iris.csv")
 
 
@@ -8,14 +6,18 @@ print(f)
 
 
 #print the contents of the file
-
-print(f.read())f = open("data/iris.csv")
-
-
-#prints the name of the file
-print(f)
-
-
-#print the contents of the file
+#reference:https://web.microsoftstream.com/video/f0788c1c-c7bd-4347-98ac-477186938ed7
 
 print(f.read())
+
+#calculate the mean of each column
+
+import numpy
+
+#read data file into array
+data = numpy.genfromtxt('data/iris.csv', delimiter=',')
+
+firstcol = data[:,0]
+mean firstcol = numpy.mean(data[:,0])
+
+print("average of first column is:", mean firstcol)
