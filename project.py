106466a67b269289c70c7b88720eb60f2e36d2b4
nualagallagher <36246540+nualagallagher@users.noreply.github.com>
2018-04-27 21:37:32 +0100

    Update project.py

--- project.py
+++ project.py
@@ -26,6 +26,7 @@ print("Please see the data contents of Fisher's Iris Data Set above  ")
 #calculate the mean of each column
 
 import numpy
+#import mathplotlib.pyplot as plt
 
 #read data file into array
 data = numpy.genfromtxt('data/iris.csv', delimiter=',')
@@ -36,64 +37,118 @@ meanfirstcol = numpy.mean(data[:,0])
 secondcol = data[:,1]
 meansecondcol = numpy.mean(data[:,1])
 
-thirdcol = data[:,1]
+thirdcol = data[:,2]
 meanthirdcol = numpy.mean(data[:,2])
 
-fourthcol = data[:,1]
+fourthcol = data[:,3]
 meanfourthcol = numpy.mean(data[:,3])
 
+
+#to print the mean of the length and width of the sepals and petals of each sample collected by Ronald Fisher
+print("the mean of sepal length in column 1 is:", meanfirstcol)
+
+print("the mean of the Sepal width in column 2 is:", meansecondcol)
+
+print("the mean of the Petal length in column 3 is:", meanthirdcol)
+
+print("the mean of the Petal width in column 4 is:", meanfourthcol)
+
+
 #to find the min of each column:
+#reference: https://web.microsoftstream.com/video/f0788c1c-c7bd-4347-98ac-477186938ed7
+#reference: https://plot.ly/numpy/min/
 
 firstcol = data[:,0]
-minfirstcol = numpy.min(data[:,0])
+minfirstcol = numpy.min(firstcol)
 
-numpy.min(secondcol)
 secondcol = data[:,1]
-minsecondcol = numpy.min(data[:,1])
+minsecondcol = numpy.min(secondcol)
 
-numpy.min(thirdcol)
 thirdcol = data[:,2]
-minthirdcol = numpy.min(data[:,2])
+minthirdcol = numpy.min(thirdcol)
 
-numpy.min(fourthcol)
 fourthcol = data[:,3]
-minfourthcol = numpy.min(data[:,3])
+minfourthcol = numpy.min(fourthcol)
 
-#to print the mean of the length and width of the sepals and petals of each sample collected by Ronald Fisher
-print("the min of sepal length in column 1 is:", meanfirstcol)
+#to print the min of the length and width of the sepals and petals of each sample collected by Ronald Fisher
+print("the min of sepal length in column 1 is:", minfirstcol)
 
-print("the min of the Sepal width in column 2 is:", meansecondcol)
+print("the min of the Sepal width in column 2 is:", minsecondcol)
 
-print("the min of the Petal length in column 3 is:", meanthirdcol)
+print("the min of the Petal length in column 3 is:", minthirdcol)
 
-print("the min of the Petal width in column 4 is:", meanfourthcol)
-
-
-#to print the mean of the length and width of the sepals and petals of each sample collected by Ronald Fisher
-print("the average of sepal length in column 1 is:", meanfirstcol)
-
-print("the average of the Sepal width in column 2 is:", meansecondcol)
-
-print("the average of the Petal length in column 3 is:", meanthirdcol)
+print("the min of the Petal width in column 4 is:", minfourthcol)
 
 
-print("the average of the Petal width in column 4 is:", meanfourthcol)
+
+#To find the max of each column
+#reference: https://web.microsoftstream.com/video/f0788c1c-c7bd-4347-98ac-477186938ed7
+#reference: https://plot.ly/numpy/min/  (same concept as finding the min of each colum)
+
+firstcol = data[:,0]
+maxfirstcol = numpy.max(firstcol)
+
+secondcol = data[:,1]
+maxsecondcol = numpy.max(secondcol)
+
+thirdcol = data[:,2]
+maxthirdcol = numpy.max(thirdcol)
+
+fourthcol = data[:,3]
+maxfourthcol = numpy.max(fourthcol)
+
+#to print the max of the length and width of the sepals and petals of each sample collected by Ronald Fisher
+print("the max of sepal length in column 1 is:", maxfirstcol)
+
+print("the max of the Sepal width in column 2 is:", maxsecondcol)
+
+print("the max of the Petal length in column 3 is:", maxthirdcol)
+
+print("the max of the Petal width in column 4 is:", maxfourthcol)
 
 
 #to create a bar chart/histogram diagram of each column
 #reference:Programming & Scripting, Matplotlib Pyplot, Ian McLaughlin (https://web.microsoftstream.com/video/f0788c1c-c7bd-4347-98ac-477186938ed7)
+#reference: https://matplotlib.org/users/pyplot_tutorial.html
+
 import matplotlib.pyplot as pl
+from matplotlib.ticker import NullFormatter
+pl.figure(1)
+
+pl.subplot(221)
 pl.hist(firstcol)
-pl.show()
+pl.title('Sepal Length')  #create Title of graph
+pl.ylabel('Samples Taken')  #to name the y axis
+pl.xlabel('Measurements (cm)')  #to name the x axis
 
 
+pl.subplot(222)
 pl.hist(secondcol)
-pl.show()
+pl.title('Sepal Width')
+pl.ylabel('Samples Taken')
+pl.xlabel('Measurements (cm)')
 
+
+pl.subplot(223)
 pl.hist(thirdcol)
-pl.show()
+pl.title('Petal Lentgh')
+pl.ylabel('Samples Taken')
+pl.xlabel('Measurements (cm)')
 
+
+pl.subplot(224)
 pl.hist(fourthcol)
+pl.title('Petal Width')
+pl.ylabel('Samples Taken')
+pl.xlabel('Measurements (cm)')
+
+
+pl.gca().yaxis.set_minor_formatter(NullFormatter())
+# Adjust the subplot layout, because the logit one may take more space
+# than usual, due to y-tick labels like "1 - 10^{-3}"
+pl.subplots_adjust(top=0.95, bottom=0.1, left=0.10, right=0.95, hspace=0.40,
+                    wspace=0.35)
+
 pl.show()
 
 
